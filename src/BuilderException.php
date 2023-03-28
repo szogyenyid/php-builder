2bc553c0ac2ec688b3d4bc4878543ab1a42c8d34
Nicolas CARPi <nico-git@deltablot.email>
2023-03-28 11:48:57 +0200

    use sprintf to avoid using antislash or double dollars

--- src/BuilderException.php
+++ src/BuilderException.php
@@ -8,11 +8,11 @@ class BuilderException extends Exception
 {
     public static function invalidMethodName(string $methodName): self
     {
-        return new self("Builder method names must start with \"with\". Invalid method name: $methodName");
+        return new self(sprintf('Builder method names must start with "with". Invalid method name: %s', $methodName));
     }
 
     public static function notSettable(string $property, string $class): self
     {
-        return new self("No property with name \"$$property\" found in class $class");
+        return new self(sprintf('No property with name: %s found in class: %', $property, $class));
     }
 }
